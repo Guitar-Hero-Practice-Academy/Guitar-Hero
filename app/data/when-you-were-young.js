904d37765d28c715d8d935d6f215e37c3695fe4f
robertjavery-byte <robert.j.avery@gmail.com>
2026-08-05 06:36:15 +1000

    send it

---
+++ app/data/when-you-were-young.js
@@ -0,0 +1,147 @@
+window.initialSongs.push({
+  id: "the-killers-when-you-were-young",
+  contentRevision: 1,
+  title: "When You Were Young",
+  artist: "The Killers",
+  album: "Sam's Town",
+  albumYear: 2006,
+  albumOrder: 3,
+  difficulty: "Intermediate",
+  tuning: "E A D G B E",
+  key: "E",
+  bpm: 130,
+  videoUrl: "",
+  chords: [
+    { name: "E", frets: "022100", fingers: "231" },
+    { name: "E*", frets: "x79997", fingers: "134121" },
+    { name: "F#", frets: "244322", fingers: "134211" },
+    { name: "G#m", frets: "466444", fingers: "134111" },
+    { name: "B", frets: "x24442", fingers: "13421" },
+    { name: "C#m", frets: "x46654", fingers: "13421" },
+    { name: "D#m", frets: "x68876", fingers: "13421" },
+    { name: "A#m", frets: "688666", fingers: "134111" }
+  ],
+  strumming: [{
+    name: "Whole song",
+    bpm: 130,
+    beats: ["D", "U", "D", "U", "D", "U", "D", "U"],
+    accents: [0, 4],
+    chordRun: "E  F#  G#m  B",
+    note: "Keep all eighth notes even and drive the changes without rushing."
+  }],
+  chart: `
+[Intro]
+E*
+| E    | F# G#m | B    | E    |
+| E    | F# G#m | B    | E    |
+
+[Verse 1]
+E                     F#    G#m
+You sit there in your heartache
+B                  E
+Waiting on some beautiful boy to
+E                    F#       G#m
+To save you from your old ways
+B
+You play forgiveness
+  E
+Watch it now, here he comes
+
+[Chorus]
+   E                         F# G#m
+He doesn't look a thing like Jesus
+     B
+But he talks like a gentleman
+G#m
+Like you imagined
+                         E       F# G#m   B    E
+When you were young
+
+[Verse 2]
+E
+Can we climb this mountain
+F#         G#m
+I don't know
+B                  E
+Higher now than ever before
+ E                              F#       G#m
+I know we can make it if we take it slow
+B
+Let's take it easy
+                    E
+Easy now, watch it go
+
+[Chorus]
+       E                              F# G#m
+We're burning down the highway skyline
+     B
+On the back of a hurricane
+E
+That started turning
+                         E       F# G#m   B    E
+When you were young
+       E       F# G#m   B    E
+When you were young
+
+[Verse 3]
+       E    F#
+And sometimes you close your eyes
+    G#m       B                         E
+And see the place where you used to live
+      E          E
+When you were young
+
+[Instrumental]
+|(E)   | F# G#m | B    | E    |
+| E    | F# G#m | B    | E    |
+| E  D#m  C#m | B  B A#m G#m | F# |(F#) |
+
+[Bridge]
+             C#m
+They say the devil's water, it ain't so sweet
+   E                              G#m
+You don't have to drink right now
+D#m                         G#m
+   But you can dip your feet
+F#                              E    F# G#m  B   E
+   Every once in a little while
+
+[Instrumental]
+| E    | F# G#m | B    | E    | x2
+
+[Verse 4]
+E                     F#    G#m
+You sit there in your heartache
+B                  E
+Waiting on some beautiful boy to
+E                    F#       G#m
+To save you from your old ways
+B
+You play forgiveness
+  E
+Watch it now, here he comes
+
+[Chorus]
+   F#
+He doesn't look a thing like Jesus
+     B
+But he talks like a gentleman
+G#m
+Like you imagined
+       E       F# G#m
+When you were young
+B
+(talks like a gentleman)
+E
+(like you imagined)
+       E       F# G#m   B    E
+When you were young
+
+[Outro]
+          E                              F# G#m  B    E
+I said he doesn't look a thing like Jesus
+   E                           F# G#m  B    E
+He doesn't look a thing like Jesus
+                        E    D#m  C#m  B  (B A#m G#m)  F#
+But more than you'll ever know`
+});
