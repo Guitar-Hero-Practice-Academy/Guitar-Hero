821d725eb8c682677a6e814fc0588f026403098f
robertjavery-byte <robert.j.avery@gmail.com>
2026-08-05 15:30:12 +1000

    launch

---
+++ app/data/keep-on-movin.js
@@ -0,0 +1,138 @@
+window.initialSongs.push({
+  id: "five-keep-on-movin",
+  contentRevision: 1,
+  title: "Keep On Movin'",
+  artist: "Five",
+  album: "Invincible",
+  albumYear: 1999,
+  albumOrder: 2,
+  difficulty: "Absolute Beginner",
+  tuning: "E A D G B E",
+  key: "G",
+  bpm: 135,
+  videoUrl: "https://www.youtube.com/watch?v=FbWJVult-9M",
+  chords: [
+    { name: "D", frets: "xx0232", fingers: "132" },
+    { name: "Em", frets: "022000", fingers: "12" },
+    { name: "C", frets: "x32010", fingers: "32 1" },
+    { name: "G", frets: "320003", fingers: "21 3" }
+  ],
+  strumming: [{
+    name: "Whole song",
+    bpm: 135,
+    beats: ["D", "", "D", "U", "", "U", "D", "U"],
+    accents: [0, 4],
+    chordRun: "D  Em  C  G",
+    note: "Keep the pulse light and even; use one strum per chord in the marked rings section."
+  }],
+  chart: `
+[Intro]
+| D    | Em   | C    | G    |
+| D    | Em   | C    | G    |
+
+[Verse 1]
+  D                          Em
+I woke up today with this feeling
+     C                          G
+That better things are coming my way.
+    D                       Em
+And if the sunshine has a meaning
+           C                           G
+Telling me not to let things get in my way
+
+[Pre-Chorus]
+D                              Em
+  When the rainy days are dying, gotta keep on, keep on trying
+C                               G
+  All the bees and birds are flying, Ahhhh...
+D                              Em
+  Never let go, gotta hold on and Non stop 'til the break of dawn and
+C                                G
+  Keep on moving, don't stop rocking, Ahhhh...
+
+[Chorus]
+       D              Em
+Get on up when you're down, baby
+C                 G
+Take a good look around
+                D              Em
+I know it's not much, but it's okay
+             C              G
+We'll keep on movin' on anyway
+
+[Interlude]
+| (D)             | (D)             |
+
+[Verse 2]
+D                              Em
+Feels like I have should be screaming
+C                              G
+Trying to get through to my friends.
+    D                               Em
+sometimes it feels that life has no meaning,
+      C                                G
+But I know things will be alright in the end.
+
+[Pre-Chorus]
+D                              Em
+  When the rainy days are dying, gotta keep on, keep on trying
+C                               G
+  All the bees and birds are flying, Ahhhh...
+D                              Em
+  Never let go, gotta hold on and Non stop 'til the break of dawn and
+C                                G
+  Keep on moving, don't stop rocking, Ahhhh...
+
+[Chorus]
+       D              Em
+Get on up when you're down, baby
+C                 G
+Take a good look around
+                D              Em
+I know it's not much, but it's okay
+             C              G
+We'll keep on movin' on anyway
+
+[Interlude]
+| (D)             | (D)             |
+
+[Pre-Chorus - rings, strum each chord once]
+D                              Em
+  When the rainy days are dying, gotta keep on, keep on trying
+C                               G
+  All the bees and birds are flying, Ahhhh...
+D                              Em
+  Never let go, gotta hold on and Non stop 'til the break of dawn and
+C                                G
+  Keep on moving, don't stop rocking, Ahhhh...
+
+[Pre-Chorus]
+D                              Em
+  When the rainy days are dying, gotta keep on, keep on trying
+C                               G
+  All the bees and birds are flying, Ahhhh...
+D                              Em
+  Never let go, gotta hold on and Non stop 'til the break of dawn and
+C                                G
+  Keep on moving, don't stop rocking, Ahhhh...
+
+[Chorus - a cappella, no chords]
+          N.C.
+Get on up when you're down, baby
+N.C.
+Take a good look around
+               N.C.
+I know it's not much, but it's okay
+             N.C.
+We'll keep on movin' on anyway
+
+[End-Chorus - repeat and fade out]
+       D              Em
+Get on up when you're down, baby
+C                 G
+Take a good look around
+                D              Em
+I know it's not much, but it's okay
+             C              G
+We'll keep on movin' on anyway`
+});
